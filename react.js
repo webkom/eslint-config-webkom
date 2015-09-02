899ea07be073d9257516da0684954eb1397c396c
ekmartin <mail@ekmartin.no>
2015-09-02 21:47:34 +0200

    Use Object.assign polyfill

--- react.js
+++ react.js
@@ -1,5 +1,6 @@
+var assign = require('object-assign');
 var baseRules = require('./base');
-var rules = Object.assign(baseRules, {
+var rules = assign(baseRules, {
   'react/jsx-quotes': 0,
   'react/sort-comp': 0, // enable when https://github.com/yannickcr/eslint-plugin-react/issues/128 is fixed
 });
