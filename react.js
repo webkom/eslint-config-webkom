fc5475b1605847ae53649aa8c5377e1eeca02623
ekmartin <mail@ekmartin.no>
2015-10-01 15:39:26 +0200

    Replace react/jsx-quotes with jsx-quotes

--- react.js
+++ react.js
@@ -1,11 +1,11 @@
 var assign = require('object-assign');
 var baseRules = require('./base');
 var rules = assign(baseRules, {
-  'react/jsx-quotes': 0,
+  'jsx-quotes': [2, 'prefer-single'],
   'react/sort-comp': 0, // enable when https://github.com/yannickcr/eslint-plugin-react/issues/128 is fixed
 });
 
 module.exports = {
   extends: 'airbnb',
   rules: rules
-}
+};
