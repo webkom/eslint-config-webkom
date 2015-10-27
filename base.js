e1ee4557348038b8008d117e61b3c832399af5a1
ekmartin <mail@ekmartin.no>
2015-10-27 18:04:57 +0100

    Update linting config

--- base.js
+++ base.js
@@ -6,5 +6,11 @@ module.exports = {
   'comma-dangle': [2, 'never'], // eslint.org/docs/rules/comma-dangle.html
   'default-case': 0, // http://eslint.org/docs/rules/default-case.html,
   'id-length': 0,
-  'func-names': 0
+  'func-names': 0,
+  'object-curly-spacing': [2, 'always'],
+  'object-shorthand': [2, 'always'],
+  'no-const-assign': 2,
+  'arrow-spacing': [2, { before: true, after: true }],
+  'prefer-spread': 2,
+  'constructor-super': 2
 };
