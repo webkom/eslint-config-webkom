8ba44b8deb9181fb7d519d633a1027974135298b
ekmartin <mail@ekmartin.no>
2015-10-30 15:25:01 +0100

    Disable object-shorthand in legacy and release 1.0.0

--- legacy.js
+++ legacy.js
@@ -1,7 +1,10 @@
 var assign = require('object-assign');
 var baseRules = require('./base');
+var rules = assign(baseRules, {
+  'object-shorthand': 0
+});
 
 module.exports = {
   extends: 'airbnb/legacy',
-  rules: baseRules
+  rules: rules
 };
