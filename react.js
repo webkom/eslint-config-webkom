54aa59c033455924f8a61f4d15d21327231614b5
ekmartin <mail@ekmartin.com>
2016-01-19 20:07:54 +0100

    Make jsx-no-bind a warning

--- react.js
+++ react.js
@@ -2,6 +2,7 @@ var assign = require('object-assign');
 var baseRules = require('./base');
 var rules = assign(baseRules, {
   'jsx-quotes': [2, 'prefer-single'],
+  'react/jsx-no-bind': 1,
   'react/sort-comp': 0, // enable when https://github.com/yannickcr/eslint-plugin-react/issues/128 is fixed
 });
 
